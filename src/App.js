88f2966ba967c2304d2d07fafcc31b8f4c8bb02d
binay parajuli <binayparajuli@binays-MacBook-Pro.local>
2022-08-21 18:09:52 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Button from "./Button"
+const App=() =>{
+    return <div>
+        <h1>Helloo</h1>
+        <Button name="Goggle" link="https://google.com"/>
+        <Button name="facebook"/>
+    </div>
+    
+}
+export default App;
